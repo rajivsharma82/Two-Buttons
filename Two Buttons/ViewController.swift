23af10d31eebfcbb430861429d4a65dd1b56d3d7
Rajeev <rajivsharma82@gmail.com>
2019-02-18 12:19:52 -0600

    Initial Commit

---
+++ Two Buttons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Two Buttons
+//
+//  Created by rajeev on 2/18/19.
+//  Copyright © 2019 rajeev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
